74b5f6701d34ae6f70fb2b4a4c7a98161c673c3c
Néstor Coppi <nestorcoppi@gmail.com>
2011-10-15 22:59:14 -0300

    Clientes en Mayuscula

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,5 @@
 class Client < ActiveRecord::Base
+  before_save :up_name
   #validates
   validates :name, :last_name, :document, :client_kind, :presence => {
     :message => 'must be present'
@@ -8,9 +9,14 @@ class Client < ActiveRecord::Base
   
   has_many :bills
   
+  #methods
   def to_s
     self.name + ' ' + self.last_name
   end
   
+  def up_name
+    self.name = self.name.split.map(&:camelize).join(' ')
+    self.last_name = self.last_name.split.map(&:camelize).join(' ')
+  end
   
 end
