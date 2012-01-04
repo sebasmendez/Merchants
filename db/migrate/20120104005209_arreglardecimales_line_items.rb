3635e28642223be1a84535d647f515d7db709891
Néstor Coppi <coppielfico@gmail.com>
2012-01-03 21:56:51 -0300

    terminando deploy

--- db/migrate/20120104005209_arreglardecimales_line_items.rb
+++ db/migrate/20120104005209_arreglardecimales_line_items.rb
@@ -1,7 +1,7 @@
 class ArreglardecimalesLineItems < ActiveRecord::Migration
   def change
     change_column :line_items, :price, :decimal, precision: 15, scale: 2
-    change_column :line_items, :quantity, :decimal, precision: 15, scale: 2
+    change_column :line_items, :quantity, :decimal, precision: 15, scale: 2, default: 1
   end
 
 end
