d56e6f29e456d8425b9de6ed2cca1410ad9b69fe
Néstor Coppi <coppielfico@gmail.com>
2012-01-06 13:30:46 -0300

    Arreglando decimales

--- db/migrate/20120106162429_arreglar_decimales_productos.rb
+++ db/migrate/20120106162429_arreglar_decimales_productos.rb
@@ -1,7 +1,7 @@
 class ArreglarDecimalesProductos < ActiveRecord::Migration
   def change
-	change_column :products, :iva, :decimal, precision: 15, scale: 2
-	change_column :products, :pricedist, :decimal, precision: 15, scale: 2
-	change_column :products, :stock, :decimal, precision: 15, scale: 2
+    change_column :products, :iva, :decimal, precision: 15, scale: 2
+    change_column :products, :pricedist, :decimal, precision: 15, scale: 2
+    change_column :products, :stock, :decimal, precision: 15, scale: 2, default: 0
   end
 end
