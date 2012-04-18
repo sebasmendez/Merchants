98d5c53a86e58b2805141216554df71bde3e4dac
Néstor Coppi <nestorcoppi@gmail.com>
2012-04-18 11:59:04 -0300

    Agregando ordenes en cliente

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -15,6 +15,7 @@ class ClientsController < ApplicationController
   # GET /clients/1.xml
   def show
     @client = Client.find(params[:id])
+    @client_orders = @client.orders.order('created_at DESC').paginate(page: params[:page], per_page: 5)
 
     respond_to do |format|
       format.html # show.html.erb
