c9d9f98fbbb08d893c73ad1f2c7208eb9975928f
Néstor Coppi <nestorcoppi@gmail.com>
2012-09-04 22:04:59 -0300

    Mini-fix

--- app/assets/javascripts/bills.js
+++ app/assets/javascripts/bills.js
@@ -1,9 +1,8 @@
 jQuery(function(){
         $('#excel_with_month').live('click', function() {
-          original_url = window.location.host + window.location.pathname;
           month = prompt('Ingrese el numero del mes');
           year = prompt('Ingrese el numero del año YYYY');
-          url = original_url + ".csv?year=" + year + "&month=" + month;
+          url = window.location.pathname + ".csv?year=" + year + "&month=" + month;
           window.location = url;
         });
         
