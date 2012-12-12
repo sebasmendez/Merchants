1f83cf5ce3f32713e2bec78b36b9480859165e24
Néstor Coppi <nestorcoppi@gmail.com>
2012-12-12 15:28:26 -0300

    Mejoras en Capistrano =)

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,4 @@
 require 'bundler/capistrano'
-default_run_options[:pty] = true
 
 set :application, "merchants"
 set :repository,  "git@github.com:Shelvak/Merchants.git"
@@ -10,9 +9,7 @@ set :scm, :git
 set :user, 'eltonel'
 # set :user, 'rotsen'
 set :deploy_via, :remote_cache
-#set :rails_env, :production
-#set :use_sudo, false
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :use_sudo, false
 
 set :branch, 'master'
 role :web, "eltonel.no-ip.org"                          # Your HTTP server, Apache/etc
@@ -25,12 +22,11 @@ role :db,  "eltonel.no-ip.org", :primary => true
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
-# If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
   task :start do ; end
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+    run "touch #{File.join(current_path,'tmp','restart.txt')}"
   end
   # desc "reload the database with seed data"
 	task :seed do
