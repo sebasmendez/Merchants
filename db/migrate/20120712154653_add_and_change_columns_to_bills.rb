de58d943f8de08eb058d33728d277f891a6e0ef1
Néstor Coppi <nestorcoppi@gmail.com>
2014-11-14 22:53:30 -0300

    Sdahlskd

--- db/migrate/20120712154653_add_and_change_columns_to_bills.rb
+++ db/migrate/20120712154653_add_and_change_columns_to_bills.rb
@@ -1,7 +1,7 @@
 class AddAndChangeColumnsToBills < ActiveRecord::Migration
   def change
-    rename_column :bills, :date, :prod_count
-    change_column :bills, :prod_count, :integer
+    rename_column :bills, :date, :old_date
+    add_column :bills, :prod_count, :integer
     add_column :bills, :discount, :decimal, precision: 15, scale: 2, default: 0
   end
 end
